0db58e51b9a6be91ea593550ce7b0123b6ff095b
whybfq <littlesanner@gmail.com>
2020-02-15 08:26:50 +0700

    improve the NAND, OR, XOR gate

--- ch02/or_gate.py
+++ ch02/or_gate.py
@@ -3,14 +3,9 @@ import numpy as np
 
 
 def OR(x1, x2):
-    x = np.array([x1, x2])
-    w = np.array([0.5, 0.5])
-    b = -0.2
-    tmp = np.sum(w*x) + b
-    if tmp <= 0:
-        return 0
-    else:
-        return 1
+    x, w, b = np.array([x1, x2]), np.array([0.5, 0.5]), -0.2
+    return 1 if np.sum(x * w) + b > 0 else 0
+
 
 if __name__ == '__main__':
     for xs in [(0, 0), (1, 0), (0, 1), (1, 1)]:
