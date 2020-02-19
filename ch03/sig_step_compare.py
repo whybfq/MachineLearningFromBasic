0873c18ec49860c016d7692f542a62f4a617f3db
whybfq <littlesanner@gmail.com>
2020-02-19 08:23:15 +0700

    add some commments about step_function

--- ch03/sig_step_compare.py
+++ ch03/sig_step_compare.py
@@ -7,8 +7,11 @@ def sigmoid(x):
     return 1 / (1 + np.exp(-x))    
 
 
-def step_function(x):
+def step_function(x):   # x is np.array
+    # y = x > 0
+    # return y.astype(np.int)
     return np.array(x > 0, dtype=np.int)
+
 
 x = np.arange(-5.0, 5.0, 0.1)
 y1 = sigmoid(x)
@@ -16,5 +19,5 @@ y2 = step_function(x)
 
 plt.plot(x, y1)
 plt.plot(x, y2, 'k--')
-plt.ylim(-0.1, 1.1) #指定图中绘制的y轴的范围
+plt.ylim(-0.1, 1.1)  # 指定图中绘制的y轴的范围
 plt.show()
