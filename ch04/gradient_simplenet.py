953ea6de810525dd53f7121cfc880d87c80b1196
whybfq <littlesanner@gmail.com>
2020-02-20 22:48:23 +0700

    improve the gradient_simplenet.py

--- ch04/gradient_simplenet.py
+++ ch04/gradient_simplenet.py
@@ -1,14 +1,16 @@
 # coding: utf-8
 import sys, os
-sys.path.append(os.pardir)  # 为了导入父目录中的文件而进行的设定
 import numpy as np
+
+sys.path.append(os.pardir)  # 为了导入父目录中的文件而进行的设定
+
 from common.functions import softmax, cross_entropy_error
 from common.gradient import numerical_gradient
 
 
 class simpleNet:
     def __init__(self):
-        self.W = np.random.randn(2,3)
+        self.W = np.random.randn(2, 3)
 
     def predict(self, x):
         return np.dot(x, self.W)
@@ -19,6 +21,7 @@ class simpleNet:
         loss = cross_entropy_error(y, t)
 
         return loss
+
 
 x = np.array([0.6, 0.9])
 t = np.array([0, 0, 1])
