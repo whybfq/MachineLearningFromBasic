866959b9a014fbb1db0925588ca8f8303243e418
whybfq <littlesanner@gmail.com>
2020-02-26 17:32:48 +0700

    add the main module in basicNeuralNetwork.py

--- ch03/basicNeuralNetwork.py
+++ ch03/basicNeuralNetwork.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 """
-one_layer_two_points.py (BSD Licensed)
+basicNeuralNetwork.py (BSD Licensed)
 © 2020 (littlesanner@gmail.com)
 """
 import numpy as np
@@ -9,6 +9,11 @@ import pyperclip
 
 c = np.pi / 2
 Bits = 100000  # hidden_size
+
+
+# every possible symbol that can be encrypted
+LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"  # 26 English alphabets
+# LETTERS = ' !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~'
 
 
 class OneLayerNet:
@@ -24,13 +29,12 @@ class OneLayerNet:
     def predict(self, x):
         W1, b1, b2 = self.params['W1'], self.params['b1'], self.params['b2']
         W2 = 1 / W1.T  # W2 is from W1
-        # print(
-        #     "The dimension of W1 is {0}\n"
-        #     "And W1 is: {1}\n"
-        #     "The dimension of W2 is {2}\n"
-        #     "And W2 is: {3}"
-        #     .format(W1.shape, W1, W2.shape, W2)
-        # )
+        print(
+            "The dimension of W1 is {d1}\n"
+            "And W1(key1) is: {W1}\n"
+            "The dimension of W2 is {d2}\n"
+            "And W2(key2) is: {W2}".format(d1=W1.shape, W1=W1, d2=W2.shape, W2=W2)
+        )
         # print('b1:{b1}\nb2: {b2}'.format(b1=b1, b2=b2))
 
         a1 = np.dot(x, W1) + b1
@@ -38,7 +42,7 @@ class OneLayerNet:
         print(
             # "The dimension of a1 is: {}\n"
             # "And a1 is: {a1}"
-            "Cipher(z1) : \n{z1}".format(a1.shape, a1=a1, z1=z1)
+            "Cipher text(z1) : \n{z1}".format(a1.shape, a1=a1, z1=z1)
         )
 
         a2 = np.dot(z1, W2) + b2
@@ -60,56 +64,50 @@ class OneLayerNet:
         return a.dot(np.linalg.inv(W.dot(1 / W.T)))  # have to use a to multiply the inverse matrix of the identity matrix
 
 
-# every possible symbol that can be encrypted
-LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"  # 26 English alphabets
-# LETTERS = ' !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~'
+def main():  # run the encryption/decryption code on each symbol in the message string
+    # deal with the input(plaintext)
+    translated = ""  # stores the encrypted/decrypted form of the message
+    # tells the program to encrypt or decrypt
+    mode = "encrypt"  # set to 'encrypt' or 'decrypt'
 
-# message = np.array([*string.ascii_lowercase])[3]  #  array(['d'], dtype='<U1')
-Inputs = "a"  # ord('a')->97,  chr(97)->a
-# capitalize the string in message
-# Inputs = Inputs.upper()
-numbers = [ord(letter) for letter in Inputs]
+    # message = np.array([*string.ascii_lowercase])[3]  #  array(['d'], dtype='<U1')
+    Inputs = "abcdeda"  # ord('a')->97,  chr(97)->a
 
-message = np.array(numbers)   # eg: np.array([[1, 2]]), np.array([[[1, 2, 3]]]),
-print("Input Plaintext {}\nAnd the dimension is: {}".format(Inputs, message.shape))
+    # Inputs = Inputs.upper()  # capitalize the string in message
+    numbers = [ord(letter) for letter in Inputs]
 
-# tells the program to encrypt or decrypt
-mode = "encrypt"  # set to 'encrypt' or 'decrypt'
+    message = np.array([numbers])  # eg: np.array([[1, 2]]), np.array([[[1, 2, 3]]]),
+    print("Input Plaintext {}\nAnd the dimension is: {}".format(Inputs, message.shape))
 
+    # cipher every alphabet in the message
+    for symbol in Inputs:
+        if symbol in LETTERS:
+            if mode == "encrypt":
+                # initialize the neural network
+                input_size, hidden_size, output_size = message.ndim, Bits, message.ndim
+                test: OneLayerNet = OneLayerNet(input_size, hidden_size, output_size)
 
-# initialize the neural network
-input_size, hidden_size, output_size = message.ndim, Bits, message.ndim
-test: OneLayerNet = OneLayerNet(input_size, hidden_size, output_size)
+                Decrypted_text = test.predict(message)
+                print('Decrypt text : {}'.format(chr(Decrypted_text.__int__())))
+            elif mode == 'decrypt':
+                pass
+            else:
+                print('mode can only be encrypt or decrypt!')
 
-Decrypted_text = test.predict(message).astype(int)
-print('Decrypt text : {}'.format(chr(Decrypted_text.__int__())))
+            # add encrypted/decrypted number's symbol at the end of translated
+            translated += Decrypted_text
 
-
-# run the encryption/decryption code on each symbol in the message string
-# stores the encrypted/decrypted form of the message
-translated = ""
-
-for symbol in Inputs:
-    if symbol in LETTERS:
-        # get the encrypted (or decrypted) number for this symbol
-        num = LETTERS.find(symbol)  # get the number of the symbol
-        if mode == 'encrypt':
-            Decrypted_text = test.predict(message).astype(int)
-        elif mode == 'decrypt':
-            pass
         else:
-            print('mode can only be encrypt or decrypt!')
+            # just add the symbol without encrypting/decrypting
+            translated += symbol
 
-        # add encrypted/decrypted number's symbol at the end of translated
-        translated += Decrypted_text
+    # print the encrypted/decrypted string to the screen
+    print("The cracking code is {}".format(translated))
 
-    else:
-        # just add the symbol without encrypting/decrypting
-        translated += symbol
+    # copy the encrypted/decrypted string to the clipboard
+    pyperclip.copy(translated)
 
-# print the encrypted/decrypted string to the screen
-print("The cracking code is {}".format(translated))
 
-# copy the encrypted/decrypted string to the clipboard
-pyperclip.copy(translated)
+if __name__ == '__main__':
+    main()
 
