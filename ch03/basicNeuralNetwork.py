5670a2ed0744669da7332649e92f001b0bf60bca
whybfq <littlesanner@gmail.com>
2020-02-26 13:13:35 +0700

    change the input of basicNeuralNetwork.py form one point to multi inputs

--- ch03/basicNeuralNetwork.py
+++ ch03/basicNeuralNetwork.py
@@ -1,5 +1,6 @@
 import numpy as np
-c = np.pi / 2
+
+# c = np.pi / 2
 Bits = 100000  # hidden_size
 
 
@@ -8,7 +9,7 @@ def h1(x):  # activation function 1
 
 
 def h2(W, a):  # W is the key1 and a is the output form last layer
-    return np.linalg.inv(W.dot(1 / W.T)).dot(a)
+    return a.dot(np.linalg.inv(W.dot(1 / W.T)))   # have to use a to multiply the inverse matrix of the identity matrix
 
 
 class OneLayerNet:
@@ -17,37 +18,49 @@ class OneLayerNet:
         # initialize the weight and bias
         self.params = dict(
             W1=weight_init_std * np.random.randn(input_size, hidden_size),
-            b1=np.zeros(hidden_size)
+            b1=np.zeros(hidden_size),
+            b2=np.zeros(output_size)  # the size if the same as input_size
         )
 
     def predict(self, x):
-        W1, b1 = self.params['W1'], self.params['b1']  # W1 is the key1, consider to add b1 inside
+        W1, b1, b2 = self.params['W1'], self.params['b1'], self.params['b2']
         W2 = 1 / W1.T  # W2 is from W1
-        # print('W1:{W1}, W2: {W2}'.format(W1=W1, W2=W2))
-        b2 = np.zeros(len(x))  # the size if the same as input_size
+        print(
+            "The dimension of W1 is {0}\n"
+            "And W1 is: {1}\n"
+            "The dimension of W2 is {2}\n"
+            "And W2 is: {3}"
+            .format(W1.shape, W1, W2.shape, W2)
+        )
         # print('b1:{b1}\nb2: {b2}'.format(b1=b1, b2=b2))
 
         a1 = np.dot(x, W1) + b1
         z1 = h1(a1)
-        print(''
-              # 'a1: {a1}\n'
-              'Cipher(z1): {z1}'.format(a1=a1, z1=z1))  # z1 is the cipher
+        print(
+            "The dimension of a1 is: {}\n"
+            "And a1 is: {a1}"
+            "Cipher(z1): {z1}".format(a1.shape, a1=a1, z1=z1)
+        )  # z1 is the cipher
 
         a2 = np.dot(z1, W2) + b2
-        y = h2(W1, a2)  # y = np.linalg.inv(W1.dot(W2)).dot(a2) is the Decrypt text
+        z2 = h2(W1, a2)  # y = np.linalg.inv(W1.dot(W2)).dot(a2) is the Decrypt text
 
-        # print('a2: {a2} \n'
-        #       'z2 (Decrypt text) : {z2}'.format(a2=a2, z2=y)
-        #       )
-        return y
+        print(
+            "The dimension of a2 is: {d1}\n"
+            "And a2 is: {a2}\n"
+            "The dimension of z2 is {d2}\n"
+            "And z2(Decrypt text) is: {z2}\n".format(d1=a2.shape, a2=a2, d2=z2.shape, z2=z2)
+        )  # z2 is the decrypted text
 
+        return z2
+
+
+plaintext = np.array([[7, 9]])
+print('Input Plaintext {} and the dimension is: {}'.format(plaintext, plaintext.shape))
 
 # initialize the neural network
-input_size, hidden_size, output_size = 1, Bits, 1
+input_size, hidden_size, output_size = plaintext.ndim, Bits, plaintext.ndim
 test: OneLayerNet = OneLayerNet(input_size, hidden_size, output_size)
-
-plaintext = np.array([97])
-print('Input Plaintext : {}'.format(plaintext))
 
 Decrypted_text = test.predict(plaintext)
 print('Decrypt text : {}'.format(Decrypted_text))
