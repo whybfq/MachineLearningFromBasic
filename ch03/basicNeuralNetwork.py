c409bad99f10869c7114d9ca56d2ad4b75267d53
whybfq <littlesanner@gmail.com>
2020-02-26 13:43:25 +0700

    a little improve of the basicNeuralNetwork.py add some inputs examples

--- ch03/basicNeuralNetwork.py
+++ ch03/basicNeuralNetwork.py
@@ -25,38 +25,38 @@ class OneLayerNet:
     def predict(self, x):
         W1, b1, b2 = self.params['W1'], self.params['b1'], self.params['b2']
         W2 = 1 / W1.T  # W2 is from W1
-        print(
-            "The dimension of W1 is {0}\n"
-            "And W1 is: {1}\n"
-            "The dimension of W2 is {2}\n"
-            "And W2 is: {3}"
-            .format(W1.shape, W1, W2.shape, W2)
-        )
+        # print(
+        #     "The dimension of W1 is {0}\n"
+        #     "And W1 is: {1}\n"
+        #     "The dimension of W2 is {2}\n"
+        #     "And W2 is: {3}"
+        #     .format(W1.shape, W1, W2.shape, W2)
+        # )
         # print('b1:{b1}\nb2: {b2}'.format(b1=b1, b2=b2))
 
         a1 = np.dot(x, W1) + b1
-        z1 = h1(a1)
+        z1 = h1(a1)  # z1 is the cipher
         print(
-            "The dimension of a1 is: {}\n"
-            "And a1 is: {a1}"
+            # "The dimension of a1 is: {}\n"
+            # "And a1 is: {a1}"
             "Cipher(z1): {z1}".format(a1.shape, a1=a1, z1=z1)
-        )  # z1 is the cipher
+        )
 
         a2 = np.dot(z1, W2) + b2
-        z2 = h2(W1, a2)  # y = np.linalg.inv(W1.dot(W2)).dot(a2) is the Decrypt text
+        z2 = h2(W1, a2)  # z2 = np.linalg.inv(W1.dot(W2)).dot(a2) is the Decrypted text
 
-        print(
-            "The dimension of a2 is: {d1}\n"
-            "And a2 is: {a2}\n"
-            "The dimension of z2 is {d2}\n"
-            "And z2(Decrypt text) is: {z2}\n".format(d1=a2.shape, a2=a2, d2=z2.shape, z2=z2)
-        )  # z2 is the decrypted text
+        # print(
+        #     "The dimension of a2 is: {d1}\n"
+        #     "And a2 is: {a2}\n"
+        #     "The dimension of z2 is {d2}\n"
+        #     "And z2(Decrypt text) is: {z2}\n".format(d1=a2.shape, a2=a2, d2=z2.shape, z2=z2)
+        # )
 
         return z2
 
 
-plaintext = np.array([[7, 9]])
-print('Input Plaintext {} and the dimension is: {}'.format(plaintext, plaintext.shape))
+plaintext = np.array([1])   # eg: np.array([[1, 2]]), np.array([[[1, 2, 3]]])
+print("Input Plaintext {} and the dimension is: {}".format(plaintext, plaintext.shape))
 
 # initialize the neural network
 input_size, hidden_size, output_size = plaintext.ndim, Bits, plaintext.ndim
