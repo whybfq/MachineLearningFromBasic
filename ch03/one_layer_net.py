5ec9e9cbcf7c5abdd8c804672e45805fbf6f9b06
whybfq <littlesanner@gmail.com>
2020-03-02 09:59:10 +0700

    improve print()

--- ch03/one_layer_net.py
+++ ch03/one_layer_net.py
@@ -6,7 +6,7 @@ one_layer_two_points.py (BSD Licensed)
 import numpy as np
 
 c = np.pi / 2
-Bits = 100000  # hidden_size
+Bits = 10  # hidden_size
 
 
 def init_network():
@@ -56,7 +56,10 @@ def forward(network: dict, x: np.array) -> object:
     W1, b1 = network['W1'], network['b1']
 
     W2 = 1 / W1.T  # W2 = network['W2']
-    print('W1:{W1}, W2: {W2}'.format(W1=W1, W2=W2))
+    print(
+        f'W1:{W1} and its dimension is {W1.ndim}, its shape {W1.shape}\n'
+        # f'W2:{W2} and its dimension is {W2.ndim}, its shape {W2.shape}\n'
+    )
     # b2 = -c * np.sum(W2)  # plan to add b2 to key2
     b2 = np.zeros(len(x))
     print('b1:{b1}\nb2: {b2}'.format(b1=b1, b2=b2))
@@ -64,7 +67,10 @@ def forward(network: dict, x: np.array) -> object:
     # layer 1
     a1 = np.dot(x, W1) + b1
     z1 = h1(a1)
-    print('a1: {a1}\nCipher (z1): {z1}'.format(a1=a1, z1=z1))  # z1 is the cipher
+    print(
+        f'a1: {a1} and its dimension is {a1.ndim}, its shape {a1.shape}\n'
+        f'Cipher (z1): {z1} and its dimension is {z1.ndim}, its shape {z1.shape}'
+    )  # z1 is the cipher
 
     # output layer
     W2 = 1 / W1.T
@@ -78,21 +84,21 @@ def forward(network: dict, x: np.array) -> object:
     return z2
 
 
-# network = init_network()
-#
-# x = np.array([23.0])   # eg: np.array([[1, 2]]), np.array([[[1, 2, 3]]])
-# print('Input Plaintext : {}'.format(x))
-#
-# y = forward(network, x)
-# print('Decrypt text : {}'.format(y))
+network = init_network()
+
+x = np.array([23.0])   # eg: np.array([[1, 2]]), np.array([[[1, 2, 3]]])
+print('Input Plaintext : {}'.format(x))
+
+y = forward(network, x)
+print('Decrypt text : {}'.format(y))
 
 
 # test for the file
-cipher_text, key1 = open("Cipher_String.txt", 'r'), open("key1_String.txt", 'r')
+# cipher_text, key1 = open("Cipher_String.txt", 'r'), open("key1_String.txt", 'r')
 # for i in cipher_text.readlines():
 #     print("Each message: ", i)
-a, k = np.asarray(cipher_text.read()), np.asarray(key1.read())
-print(f"a is {a}\nkey is {k}")
+# a, k = np.asarray(cipher_text.read()), np.asarray(key1.read())
+# print(f"a is {a}\nkey is {k}")
 # for i in key1.readlines():
 #     print("Each key: ", i)
 
