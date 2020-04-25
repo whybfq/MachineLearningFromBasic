e58d6cf2ed639c31a62bcf56dc0127dca153e9a3
whybfq <littlesanner@gmail.com>
2020-04-25 10:31:15 +0700

    print(sys.getdefaultencoding())

--- ch01/img_show.py
+++ ch01/img_show.py
@@ -3,7 +3,18 @@ import matplotlib.pyplot as plt
 from matplotlib.image import imread
 import codecs
 
-img = imread('../dataset/lena.png') #读入图像
-plt.imshow(img)
+import sys
+print(sys.getdefaultencoding())
 
-plt.show()
+img = imread('../dataset/lena.png') #读入图像
+content = open("../dataset/test.txt", "r").read()
+
+# filehandle.close()
+# if content[:3] == codecs.BOM_UTF8:
+#     content = content[3:]
+# print(content.decode("utf-8"))
+
+# plt.imshow(img)
+#
+# plt.show()
+
